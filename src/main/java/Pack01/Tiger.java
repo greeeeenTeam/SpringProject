bca789c83ee82d3e45eb6e4d70d3a44de26b6a59
JiyoungHyun <shyunji023@gmail.com>
2022-03-11 10:51:42 +0900

    초기설정

---
+++ src/main/java/Pack01/Tiger.java
@@ -0,0 +1,17 @@
+package Pack01;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Tiger {
+	@RequestMapping("/t1")
+	// t1
+	String func01() {
+		System.out.println("호랑이1");
+		return "TigerView";
+	}
+
+}
