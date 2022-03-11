c519324d5bec8cf81bbf8bcf3f6b10362318e65e
ikhan-Kim <coik0831@gmail.com>
2022-03-11 13:37:31 +0900

    login delete example

--- src/main/java/Pack01/Auth.java
+++ src/main/java/Pack01/Auth.java
@@ -3,52 +3,36 @@ package Pack01;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Auth {
-   public Boolean isMember(String id, String pwd)
-   {
-      Boolean isCheck = null;
-      
-      String sql = "SELECT exists (select * from user where id=? and pwd=?) as isMember;";
-      try {
-         Connection conn = ConnectionProvider.getConnection();
-         PreparedStatement pstmt = conn.prepareStatement(sql);
-         
-         pstmt.setString(1, id);
-         pstmt.setString(2, pwd);
-         ResultSet rs = pstmt.executeQuery();
+	public Boolean login(String name, String rrn) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "INSERT INTO member values(?, ?, NULL, 0)";
+		try {
+			conn = ConnectionProvider.getConnection();
+			pstmt = conn.prepareStatement(sql);
 
-         if(rs.next())
-         {
-            isCheck = rs.getBoolean(1);
-         }
-         
-      }catch (Exception e) {
-         // TODO: handle exception
-         System.out.println(e.getMessage());
-      }
-      return isCheck;
-   }
-   
-   public Boolean login(String name, String rrn) {
-	   String sql = "INSERT INTO member values(?, ?, NULL, 0)";
-	   try {
-	         Connection conn = ConnectionProvider.getConnection();
-	         PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "김익한");
+			pstmt.setString(2, "950831");
+			int rs = pstmt.executeUpdate();
 
-	         pstmt.setString(1, "김익한");
-	         pstmt.setString(2, "950831");
-	         int rs = pstmt.executeUpdate();
-
-	         if(rs>=1)
-	         {
-	            return true;
+			if(rs>=1)
+			{
+				return true;
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}finally {
+			try{
+	            if(pstmt!=null) pstmt.close();
+	            if(conn!=null) conn.close();
+	         }catch(SQLException se){
+	            System.out.println(se.getMessage());
 	         }
-
-	      }catch (Exception e) {
-	         // TODO: handle exception
-	         System.out.println(e.getMessage());
-	      }
-	      return false;
-   }
+		}
+		return false;
+	}
 }
